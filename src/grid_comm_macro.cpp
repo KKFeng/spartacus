3a9b922338f200b67511b7663a54cdd080a6a99c
fengkai <fengkaikai456@gmail.com>
2021-12-21 13:20:09 +0800

    fix grid->hashfilled = 1 after rehash()

--- src/grid_comm_macro.cpp
+++ src/grid_comm_macro.cpp
@@ -305,7 +305,10 @@ void GridCommMacro::acquire_macro_comm_list_near()
     irregular->exchange_variable(sbuf, sizelist, rbuf);
     memory->destroy(recvicelllist);
     memory->create(recvicelllist, nrecvcell,"GridCommMacro:recvicellist");
-    if (!grid->hashfilled) grid->rehash(); // need to be done when create_grid
+    if (!grid->hashfilled) {
+        grid->rehash(); // need to be done when create_grid
+        grid->hashfilled = 1;
+    }
     for (int i = 0; i < nrecvcell; ++i) {
         cellint id = 0;
         memcpy(&id, rbuf + i * sizeof(CommMacro), sizeof(cellint));
